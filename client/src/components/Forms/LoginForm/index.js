5f63113a86e7f8b013ab7dd670e08fbad2304372
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2021-05-17 17:51:39 +0300

    Update styles login page

--- client/src/components/Forms/LoginForm/index.js
+++ client/src/components/Forms/LoginForm/index.js
@@ -1,40 +1,148 @@
 import React from "react";
-
-import { Formik, Form, Field } from "formik";
-import { Button } from "reactstrap";
-import { ReactstrapInput } from "reactstrap-formik";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import * as path from "../../../constants/routes";
+import Button from "@material-ui/core/Button";
 import { LoginSchema } from "./validation";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import InputLabel from "@material-ui/core/InputLabel";
 
-export const LoginForm = ({ login }) => (
-  <Formik
-    initialValues={{
-      username: "",
-      password: ""
-    }}
-    validationSchema={LoginSchema}
-    onSubmit={login}
-  >
-    {() => (
-      <div className="card">
-        <div className="card-header">Login</div>
-        <div className="card-body">
+const useStyles = makeStyles({
+  root: {
+    width: "392px",
+    zIndex: 10,
+    padding: "65px",
+    borderRadius: "32px",
+    border: "none",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "30px",
+    lineHeight: "47px",
+    textAlign: "center",
+    marginBottom: "25px",
+  },
+  field: {
+    marginBottom: "10px",
+  },
+  label: {
+    color: "#212429",
+    fontSize: "12px",
+    fontWeight: "normal",
+  },
+  link: {
+    textDecoration: "underline",
+    fontSize: "12px",
+  },
+  textField: {
+    "& .MuiOutlinedInput-root": {
+      borderRadius: "12px",
+    },
+  },
+  button: {
+    marginTop: "30px",
+    backgroundColor: "#4776E6",
+    minWidth: "165px",
+    marginBottom: "10px",
+  },
+  subTitle: {
+    color: "#212429",
+    fontSize: "12px",
+    fontWeight: "normal",
+    textAlign: "center",
+  },
+});
+
+export const LoginForm = ({ login }) => {
+  const classes = useStyles();
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        password: "",
+      }}
+      validationSchema={LoginSchema}
+      onSubmit={login}
+    >
+      {({ values: { username, password }, touched, errors, setFieldValue }) => (
+        <Card variant="outlined" className={classes.root}>
+          <Typography variant="h3" className={classes.title}>
+            Sign in
+          </Typography>
           <Form>
-            <Field
-              name="username"
-              type="email"
-              component={ReactstrapInput}
-              label="Email"
-            />
-            <Field
-              name="password"
-              type="password"
-              component={ReactstrapInput}
-              label="Password"
-            />
-            <Button type="submit">Submit</Button>
+            <Box className={classes.field}>
+              <InputLabel htmlFor="username" className={classes.label}>
+                Email
+              </InputLabel>
+              <TextField
+                id="username"
+                name="username"
+                helperText={touched.username ? errors.username : ""}
+                error={touched.username && Boolean(errors.username)}
+                value={username}
+                onChange={(e) => {
+                  setFieldValue("username", e.target.value);
+                }}
+                fullWidth
+                variant="outlined"
+                size="small"
+                className={classes.textField}
+              />
+            </Box>
+            <Box className={classes.field}>
+              <InputLabel htmlFor="password" className={classes.label}>
+                Password
+              </InputLabel>
+              <TextField
+                id="password"
+                name="password"
+                helperText={touched.password ? errors.password : ""}
+                error={touched.password && Boolean(errors.password)}
+                type={"password"}
+                value={password}
+                onChange={(e) => {
+                  setFieldValue("password", e.target.value);
+                }}
+                fullWidth
+                variant="outlined"
+                size="small"
+                className={classes.textField}
+              />
+            </Box>
+            <Box>
+              <Link to={path.FORGOT_PASSWORD} className={classes.link}>
+                Forgot password?
+              </Link>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignContent="center"
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                disableElevation
+                type="submit"
+                className={classes.button}
+              >
+                Sign in
+              </Button>
+              <Typography variant="h6" className={classes.subTitle}>
+                Don’t Have An Account?{" "}
+                <Link to={path.SIGN_UP} className={classes.link}>
+                  Sign up
+                </Link>
+              </Typography>
+            </Box>
           </Form>
-        </div>
-      </div>
-    )}
-  </Formik>
-);
+        </Card>
+      )}
+    </Formik>
+  );
+};
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Container } from "reactstrap";
 import { LoginForm } from "../../components/Forms/LoginForm/index";
 import { login } from "../../store/actions/profile";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    position: "relative",
+  },
+  background: {
+    height: "120%",
+    width: "432px",
+    backgroundColor: "#EEF1F7",
+    transform: "rotate(15deg)",
+    position: "absolute",
+    marginLeft: "135px",
+  },
+});
 
 const Login = (props) => {
   const dispatch = useDispatch();
+  const classes = useStyles();
   const handleLogin = async (values) => {
     const moveTo = () => {
       props.history.push("/dashboard");
@@ -13,9 +36,10 @@ const Login = (props) => {
     dispatch(login(values, moveTo));
   };
   return (
-    <Container>
+    <Box className={classes.root}>
+      <div className={classes.background}></div>
       <LoginForm login={handleLogin}></LoginForm>
-    </Container>
+    </Box>
   );
 };
 
